5370bc62f46c09704243195ec1d86b5485a60757
Chris Michael <95812665+descmd1@users.noreply.github.com>
2025-01-19 09:49:46 +0100

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -12,7 +12,7 @@ const taskSchema = new mongoose.Schema(
     },
     category: {
       type: String,
-      enum: ['work', 'personal', 'urgent'],
+      enum: ['Work', 'Personal', 'Urgent'],
       required: true,
     },
     completed: {
